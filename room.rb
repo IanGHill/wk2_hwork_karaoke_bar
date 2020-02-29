87efacb1f8536d144e4b13e8165a353b19c0ac38
IanGHill <ian.g.hill@outlook.com>
2020-02-29 11:43:08 +0000

    Second commit

--- room.rb
+++ room.rb
@@ -1,6 +1,6 @@
 class Room
 
-  attr_reader :room_name, :room_capacity, :room_entry_fee
+  attr_reader :room_name, :room_capacity, :room_entry_fee, :room_total_bar_takings, :room_total_entry_fees
 
   def initialize(room_name, room_capacity, room_entry_fee)
     @room_name = room_name
@@ -8,8 +8,63 @@ class Room
     @room_entry_fee = room_entry_fee
     @room_drink_selection = []
     @room_song_selection = []
+    @room_guest_list = []
     @room_total_bar_takings = 0
     @room_total_entry_fees = 0
   end
+
+  def get_room_head_count
+    return @room_guest_list.length
+  end
+
+  def get_num_of_songs_available
+    return @room_song_selection.length
+  end
+
+  def add_song(song)
+    @room_song_selection << song
+  end
+
+  def cust_checks_song_list(customer)
+
+    song_found = false
+
+    @room_song_selection.each do |song|
+
+      if song.song_name == customer.cust_fav_song
+        song_found = true
+      end
+    end
+
+    if song_found
+      customer.customer_cheers
+    else
+      puts "Customer grumbles"
+      return "Customer grumbles"
+    end
+  end
+
+  def collect_entry_fee(customer)
+    customer.cust_spends_cash(@room_entry_fee)
+    @room_total_entry_fees += @room_entry_fee
+  end
+
+  def check_guest_in(customer)
+    if get_room_head_count < @room_capacity
+      if customer.cust_funds_available >= @room_entry_fee
+        collect_entry_fee(customer)
+        @room_guest_list << customer
+      else
+        puts "Entry denied - insufficient funds!"
+      end
+    else
+      puts "Room already full!"
+    end
+  end
+
+  def check_guest_out(customer)
+    @room_guest_list.delete(customer)
+  end
+
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -13,8 +13,11 @@ class TestRoom < Minitest::Test
     @cust1 = Customer.new("Bill", "Poison", 50)
     @cust2 = Customer.new("Jack", "Livin on a Prayer", 60)
     @cust3 = Customer.new("Sarah", "Highway to Hell", 80)
+    @cust4 = Customer.new("Broke", "Aint Gotta Dime", 0)
 
     @song1 = Song.new("Livin on a Prayer")
+    @song2 = Song.new("Poison")
+    @song3 = Song.new("Highway to Hell")
 
     @drink1 = Drink.new("Mojito", 5)
     @drink2 = Drink.new("Long Island Ice Tea", 6)
@@ -38,5 +41,84 @@ class TestRoom < Minitest::Test
   def test_room_entry_fee
     assert_equal(14, @room3.room_entry_fee)
   end
+
+  def test_get_room_head_count
+    @room1.check_guest_in(@cust1)
+    assert_equal(1, @room1.get_room_head_count)
+  end
+
+  def test_get_num_of_songs_available
+    @room1.add_song(@song1)
+    @room1.add_song(@song2)
+    assert_equal(2,@room1.get_num_of_songs_available)
+  end
+
+  def test_add_song
+    @room2.add_song(@song1)
+    @room2.add_song(@song2)
+    @room2.add_song(@song3)
+    assert_equal(3,@room2.get_num_of_songs_available)
+  end
+
+  def test_cust_checks_song_list__fav_found
+    @room2.add_song(@song1)
+    @room2.add_song(@song2)
+    @room2.add_song(@song3)
+    assert_equal("Woohoo!", @room2.cust_checks_song_list(@cust1))
+  end
+
+  def test_cust_checks_song_list__fav_not_found
+    @room2.add_song(@song1)
+    @room2.add_song(@song1)
+    @room2.add_song(@song1)
+    assert_equal("Customer grumbles", @room2.cust_checks_song_list(@cust1))
+  end
+
+  def test_collect_entry_fee
+    @room1.collect_entry_fee(@cust1)
+    assert_equal(40, @cust1.cust_funds_available)
+  end
+
+  def test_check_guest_in_under_capacity
+    @room1.check_guest_in(@cust1)
+    assert_equal(1, @room1.get_room_head_count)
+  end
+
+  def test_check_guest_in_reach_capacity
+    @room1.check_guest_in(@cust1)
+    @room1.check_guest_in(@cust2)
+    assert_equal(2, @room1.get_room_head_count)
+  end
+
+  def test_check_guest_in_over_capacity
+    @room1.check_guest_in(@cust1)
+    @room1.check_guest_in(@cust2)
+    @room1.check_guest_in(@cust3)
+    assert_equal(2, @room1.get_room_head_count)
+  end
+
+  def test_check_guest_out
+    @room1.check_guest_in(@cust1)
+    @room1.check_guest_in(@cust2)
+    @room1.check_guest_out(@cust1)
+    assert_equal(1, @room1.get_room_head_count)
+  end
+
+  def test_check_guest_in_cust_has_enough_money
+    @room1.check_guest_in(@cust1)
+    assert_equal(10, @room1.room_total_entry_fees)
+    assert_equal(40, @cust1.cust_funds_available)
+  end
+
+  def test_collect_entry_fee_cust_insufficient_funds
+    @room1.check_guest_in(@cust4)
+    assert_equal(0, @room1.room_total_entry_fees)
+    assert_equal(0, @cust4.cust_funds_available)
+  end
+
+  def test_sell_drink_to_customer_enough_money
+    
+  end
+
 
 end
